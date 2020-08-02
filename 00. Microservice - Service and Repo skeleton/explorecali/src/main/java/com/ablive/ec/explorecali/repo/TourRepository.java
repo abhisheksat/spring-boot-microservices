ad4238171472a68645272328ff67f104fd53515a
abhisheksat <abhisheksatpute11@gmail.com>
2020-08-02 14:46:35 -0400

    Microservice skeleton - Service and Repo layers

---
+++ 00. Microservice - Service and Repo skeleton/explorecali/src/main/java/com/ablive/ec/explorecali/repo/TourRepository.java
@@ -0,0 +1,9 @@
+package com.ablive.ec.explorecali.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ablive.ec.explorecali.domain.Tour;
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+}
